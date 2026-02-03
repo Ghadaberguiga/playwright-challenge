980a498831fe0461b00ce3a4976ae78e7061402f
benrguiga <ghada.benrguiga@softtodo.com>
2026-02-03 11:29:04 +0100

    login test

---
+++ tests/purchase.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test('Login', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+
+  // fill username
+  await page.getByPlaceholder('Username').fill('standard_user');
+
+    // fill password
+  await page.getByPlaceholder('password').fill('secret_sauce');
+
+      // submit
+  await page.locator('[data-test="login-button"]').click();
+
+  // Expect a title "to contain" a substring.
+  await expect(page.getByText('Swag Labs')).toBeVisible();
+
+});
